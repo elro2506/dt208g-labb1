e184cf5e3864a33d57976c9741c20039d01ea664
Elin Ronda <elro2506@student.miun.se>
2026-04-07 10:24:05 +0200

    Lagt till tabeller och sett till så att innehåll pushas dit. Kan även radera poster

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,23 @@ const tableEl = document.querySelector("table") as HTMLTableElement;
 
 let courses: courseInformation[] = [];
 
+const savedCourses = localStorage.getItem("courses");
+if (savedCourses) {
+    courses = JSON.parse(savedCourses);
+
+    courses.forEach(course => {
+    tbodyEl.innerHTML += `
+            <tr>
+                <td>${course.courseCode}</td>
+                <td>${course.courseName}</td>
+                <td>${course.progression}</td>
+                <td>${course.syllabus}</td>
+                <td><button class="deleteBtn">Delete</button></td>
+            </tr>
+        `;
+});
+}
+
 function onAddWebsite(e: Event): void {
     e.preventDefault();
 
@@ -29,6 +46,8 @@ function onAddWebsite(e: Event): void {
     };
 
     courses.push(course);
+
+    localStorage.setItem("courses", JSON.stringify(courses));
  
 
       tbodyEl.innerHTML += `
@@ -40,16 +59,23 @@ function onAddWebsite(e: Event): void {
                 <td><button class="deleteBtn">Delete</button></td>
             </tr>
         `;
+
+        if (courses.some(course => course.courseCode === courseCode)) {
+            alert("Kursen finns redan!");
+            return;
+        }
 }
 
 function onDeleteRow(e: Event): void {
     const target = e.target as HTMLElement;
 
-    if (!target.classList.contains("deleteBtn")) {
-        return;
-    }
+    if (!target.classList.contains("deleteBtn")) return;
+    const row = target.closest("tr") as HTMLTableRowElement;
+    const index = row.rowIndex -1;
 
-    target.closest("tr")?.remove();
+    courses.splice(index, 1);
+    localStorage.setItem("courses", JSON.stringify(courses));
+row.remove();
 }
 
 formEl.addEventListener("submit", onAddWebsite);
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+base: '/dt208g-labb1/',
+});
